63c900cba0c132c77820904bb758a51e6c231243
Adeelmalik1 <adeeladi0014@gmail.com>
2022-05-11 14:46:08 +0500

    sass-added

---
+++ my-pro/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+         <header>
+              <nav>
+                <h1>Sass Site</h1>
+                <ul>
+                  <li>
+                    Home
+                  </li>
+                  <li>
+                    About
+                  </li>
+                  <li>
+                    Details
+                  </li>
+                  <li>
+                      Contact
+                  </li>
+                </ul>
+              </nav>
+         </header>
+    </div>
+  );
+}
+
+export default App;
